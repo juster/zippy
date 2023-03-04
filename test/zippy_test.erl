baf83b608010755f0efbaf98dedcd3ec48c5563d
Justin Davis <jrcd83@gmail.com>
2023-03-04 15:28:21 -0500

    Adds tests for decoding backslash escapes in strings

--- test/zippy_test.erl
+++ test/zippy_test.erl
@@ -6,3 +6,15 @@ object_key_dups_test() ->
         #{<<"dup">> => 1},
         zippy:json_to_term(<<"{\"dup\": 1, \"dup\": 2, \"dup\": 3}">>)
     ).
+
+str_esc_test() ->
+    ?assertEqual(
+        <<"\\/", 10, 13, 9, 12, 8, "\"">>,
+        zippy:json_to_term(<<"\"\\\\\\/\\n\\r\\t\\f\\b\\\"\"">>)
+    ).
+
+str_esc_u_test() ->
+    ?assertEqual(
+        [<<16#FFFF/utf8>>],
+        zippy:json_to_term(<<"[\"\\uFFFF\"]">>)
+    ).
