a53221bb3cf0fcc0edee1b6014617482271d5c40
Justin Davis <jrcd83@gmail.com>
2023-03-04 21:39:52 -0500

    Load .so file from under priv/ dir

--- src/zippy.erl
+++ src/zippy.erl
@@ -4,7 +4,20 @@
 -on_load(init/0).
 
 init() ->
-    ok = erlang:load_nif("./zippy", 0).
+    Path = filename:join(priv_path(), "zippy"),
+    ok = erlang:load_nif(Path, 0).
 
 json_to_term(_) ->
     exit(nif_library_not_loaded).
+
+priv_path() ->
+    case code:lib_dir(?MODULE) of
+        {error, badname} ->
+            priv_path1();
+        Path ->
+            filename:join(Path, "priv")
+    end.
+
+priv_path1() ->
+    Path = code:which(?MODULE),
+    filename:absname_join(filename:dirname(Path), "../priv").
